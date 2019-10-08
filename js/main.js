44eb412b0bfe6b8154f7a25166fc17fbb9255fc1
Rakesh <rakesh.sasidharan@gmail.com>
2019-10-08 14:29:32 -0400

    New project

--- js/main.js
+++ js/main.js
@@ -1,59 +1,131 @@
-/*
-*    main.js
-*    Mastering Data Visualization with D3.js
-*    Project 1 - Star Break Coffee
-*/
 
-var svgHeight = 500
-var svgWidth = 500
+var margin = {top: 100, bottom: 100, left: 10, right : 10} ; 
+var canvasHeight = 500
+var canvasWidth = 800
+var chartHeight= canvasHeight - margin.top - margin.bottom
+var chartWidth = canvasWidth - margin.left - margin.right
 
-d3.json("data/revenues.json").then(function(data){
-	data.forEach(function(d){
-		d.revenue = +d.revenue;
-		d.profit = +d.profit;
+
+var tip = d3.tip()
+	.html(function(d) {
+		return "State: " + d.properties.name + " <br>Region: " + stateRegion.get(d.properties.name) + "<br>Earthquakes: " + earthquake.get(d.properties.name) ; 
+  })
+
+
+
+var svg = d3.select("#chart-area")
+	.append("svg")
+	.attr("height", canvasHeight)
+	.attr("width", canvasWidth)
+
+svg.call(tip) ; 
+
+var projection =  d3.geoAlbersUsa()
+		.translate([canvasWidth	/2, canvasHeight/2])
+		.scale([1000])
+
+
+var earthquake =  d3.map(); 
+var stateRegion = d3.map() ; 
+var path = d3.geoPath()
+		.projection(projection) ; 
+
+var legendSize = {height: 20, width: 30}
+var legendPos = {x: 10, y: 100}
+
+var y = d3.scaleLinear()
+	.domain([0,9])
+	.range([10, 11 * legendSize.height])
+
+var colorRange = ["#fff5f0","#fee0d2","#fcbba1","#fc9272","#fb6a4a","#ef3b2c","#cb181d","#a50f15","#67000d"]
+
+var legend = d3.select("#chart-area")
+	.append("svg")
+	.attr("class", "key")
+	.attr("height", canvasHeight)
+	.attr("width", 200)
+	
+var promises = [
+	d3.json("data/states-10m.json"), 
+	d3.csv("data/state-earthquakes.csv")] 
+
+
+Promise.all(promises).then(function(data){
+	ready(data) ; 
+}).catch(function(error){
+	console.log(error)
+})
+
+
+function ready(us) {
+	us[1].forEach(function(d){
+		earthquake.set(d.States, +d["Total Earthquakes"]); 
+		stateRegion.set(d.States, d.Region); 
 	})
 
-	var svg = d3.select("#chart-area")
-				.append("svg")
-				.attr("width", svgWidth)
-				.attr("height", svgHeight)
+	var max = Math.ceil(d3.max(earthquake.values())/1000) * 1000  ;
 
-	var x = d3.scaleBand ()	
-				.domain(data.map(function(d){
-					return d.month ;
-				}))
-				.range([0, svgWidth])				
-				.paddingInner(0.3)
-				.paddingOuter(0.3)
+	var logScale = d3.scaleLog().domain([1, max])
 
-	var y = d3.scaleLinear()
-				.domain([0, d3.max(data, function(d){
-					return d.revenue ;
-				})])
-				.range([0, svgHeight])				
+	var color = d3.scaleSequential( function(d) {return d3.interpolateReds(logScale(d))})
 
-	var colr = d3.scaleOrdinal()
-				.domain(data.map(function(d){
-					return d.month;
-				}))
-				.range(d3.schemeCategory10)
+	var forInversion = [] ; 
+
+	for (let i = 1; i <= 9; i++){
+		forInversion.push(i/9); 
+	}
+
+	legendData = forInversion.map(function(d){
+		return logScale.invert(d); 
+	})
+
+	legend.selectAll("rect")
+		.data(legendData)	
+		.enter().append("rect")
+			.attr("height", legendSize.height)
+			.attr("x", legendPos.x)
+			.attr("y", function(d,i){return legendPos.y+ y(i) + 10 })
+			.attr("width", legendSize.width )
+			.attr("fill", function(d){return color(d); })
 	
-	var bars = svg.selectAll("rect").data(data)
-	
-	var bar = bars.enter()
-				.append("rect")
-				.attr("x", function(d){
-					return x(d.month)
-				})			
-				.attr("y", function(d){
-					return svgHeight - y(d.revenue)
-				})
-				.attr("width", x.bandwidth)
-				.attr("height", function(d){
-					return y(d.revenue)
-				})
-				.attr("fill", function(d){
-					return colr(d.month);
-				})
+	legend.selectAll("text")
+		.data(legendData)	
+		.enter().append("text")
+			.attr("x", legendPos.x + legendSize.width + 10)
+			.attr("y", function(d,i){return legendPos.y + y(i) + 25 })
+			.text(function(d,i){return Math.round(legendData[i]) })
+			.attr("font-size", "10px")
+			.attr("font-weight", "bold")
 
-})
+	legend.append("text")
+		.attr("class", "caption")
+		.attr("x", legendPos.x)
+		.attr("y", y.range()[0] + legendPos.y )
+		.attr("fill", "#000")
+		.attr("text-anchor", "start")
+		.attr("font-weight", "bold")
+		.attr("font-size", "15px")
+		.text("Earthquake Frequency")
+
+		var statemap = svg.append("g")
+		.attr("class", "states")
+		.selectAll("path")
+			.data(topojson.feature(us[0], us[0].objects.states).features)
+		.enter().append("path")
+			.style("fill", function(d){
+				return color(earthquake.get(d.properties.name));
+				})
+			.attr("d", path)
+			.on("mouseover",function(d, i){
+				var rect = this;
+				tip.show(d,i);
+				tip.attr("class", function(){
+					if (rect.getBBox().y < 75) 
+						return "d3-tip n down"
+					else
+						return "d3-tip n" ; 
+				})
+			})
+			.on("mouseout", tip.hide)
+			.append("title")
+}
